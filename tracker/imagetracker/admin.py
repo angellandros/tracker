60559eca567dd3f1ff647b781f71c64583e65b3f
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2018-01-07 04:32:51 +0100

    Admin

--- tracker/imagetracker/admin.py
+++ tracker/imagetracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ImageHash, ImageVisit
+
+
+class ImageHashAdmin(admin.ModelAdmin):
+    list_display = ('hash', 'location', 'header')
+    fields = ['hash', 'location', 'header']
+
+
+class ImageVisitAdmin(admin.ModelAdmin):
+    list_display = ('image', 'ip', 'data')
+    fields = ['image', 'ip', 'data']
+
+
+admin.site.register(ImageHash, ImageHashAdmin)
+admin.site.register(ImageVisit, ImageVisitAdmin)
